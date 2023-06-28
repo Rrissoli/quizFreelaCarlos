b229469526715f6674ab0bc38ac46f1ce63a0520
Rrissoli <raphels123@gmail.com>
2023-06-28 17:20:45 -0300

    capacitando estilo

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Link from 'next/link'
 
-export default function Home() {
-  
 
+export default function Home() {
+ 
   
   return (
     <main className={styles.main}>
