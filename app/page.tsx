cb53a480d02977711aaea20f7571da58c842fb92
Rrissoli <raphels123@gmail.com>
2023-06-27 21:29:16 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,9 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.box}>
-      <h1 className={styles.texto}><span className={styles.vermelho1}>Desafio do comandante</span></h1>
+      <h1 className={styles.texto}><span className={styles.vermelho1}><strong>Desafio do comandante</strong></span></h1>
       <p  className={styles.texto}>@schayanezen</p>
-      <p className={styles.texto}><strong>Qualquer pessoa pode começar um negócio, mas só os diferenciados chegarão ao topo!</strong> </p>
+      <p className={styles.texto}><strong>Qualquer pessoa pode começar um negócio, mas só os diferenciados chegarão ao topo! </strong></p>
       <p className={styles.texto}>Está na hora de descobrir por que a sua empresa não está navegando como você gostaria.</p>
       <p className={styles.texto}>Este teste é para aqueles que querem alcançar muito mais! </p>
       <p className={styles.texto}>Ao final, veja quais os desafios, oportunidades e os seus próximos passos. Aproveite essa viagem, nos vemos do outro lado.</p>
